8cb7236081ab49b424053d38b9e3681749998407
Shashank Shet <shashankshetk@gmail.com>
2023-03-21 22:34:51 +0530

    inital commit for singlyLinkedList

---
+++ LinkedLists/SinglyLinkedLists/src/SinglyLinkedList.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class SinglyLinkedList {
+    private ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+     SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+     singlyLinkedList.head = new ListNode(10);
+     ListNode second = new ListNode(20);
+     ListNode third = new ListNode(30);
+     singlyLinkedList.head.next = second;
+     second.next = third;
+
+    }
+}

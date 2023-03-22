f60ea7bfaa39bd5c4077ac1d399078ac90ebeaa4
Shashank Shet <shashankshetk@gmail.com>
2023-03-22 22:36:47 +0530

    printing linkedLists code

--- LinkedLists/SinglyLinkedLists/src/SinglyLinkedList.java
+++ LinkedLists/SinglyLinkedLists/src/SinglyLinkedList.java
@@ -12,6 +12,15 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+    //todo function to print the elements in linkedList
+    public static void printList(ListNode head){
+        ListNode current = head;
+        while (current!=null){
+            System.out.print(current.data+" -->");
+            current=current.next;
+        }
+        System.out.print("null");
+    }
     public static void main(String[] args) {
      SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
      singlyLinkedList.head = new ListNode(10);
@@ -19,6 +28,8 @@ public class SinglyLinkedList {
      ListNode third = new ListNode(30);
      singlyLinkedList.head.next = second;
      second.next = third;
+     //print the linkedList
+     printList(singlyLinkedList.head);
 
     }
 }

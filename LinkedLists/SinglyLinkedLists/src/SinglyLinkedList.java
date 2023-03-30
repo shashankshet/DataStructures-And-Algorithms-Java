c746d75055738617d6957a85f9d7d16042133cdb
Shashank Shet <shashankshetk@gmail.com>
2023-03-30 22:33:08 +0530

    adding 2 linked list|

--- LinkedLists/SinglyLinkedLists/src/SinglyLinkedList.java
+++ LinkedLists/SinglyLinkedLists/src/SinglyLinkedList.java
@@ -329,6 +329,28 @@ public class SinglyLinkedList {
         }
         return dummy.next;
     }
+
+    //todo: add two linked list
+    public ListNode addList(ListNode a, ListNode b){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int carry = 0;
+        while(a!=null || b!=null){
+            int x = (a!=null) ? a.data:0;
+            int y = (b!=null) ? b.data:0;
+            int sum = carry + x + y;
+            carry = sum/10;
+            tail.next = new ListNode(sum%10);
+            tail=tail.next;
+            if(a!=null)a=a.next;
+            if(b!=null)b=b.next;
+
+        }
+        if(carry>0){
+            tail.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
     public static void main(String[] args) {
      SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
      singlyLinkedList.head = new ListNode(10);

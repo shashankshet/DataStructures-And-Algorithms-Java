486a842f83fea7b015c25c5fbb9bad4056bb439a
Shashank Shet <shashankshetk@gmail.com>
2023-04-01 22:35:42 +0530

    DoublyLinkedList

--- LinkedLists/DoublyLinkedList/src/DoublyLinkedList.java
+++ LinkedLists/DoublyLinkedList/src/DoublyLinkedList.java
@@ -1,20 +1,21 @@
-public class DoublyLinkedList {
-    private static ListNode head;
+public class DoublyLinkedList{
+    private ListNode head;
     private ListNode tail;
-    private int length;
+    int length;
 
-    private static class ListNode{
+    private class ListNode{
         private int data;
         private ListNode next;
         private ListNode previous;
+
         private ListNode(int data){
-            this.data=data;
+            this.data = data;
         }
     }
     public DoublyLinkedList(){
-        this.head=null;
-        this.tail=null;
-        this.length=0;
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
     public boolean isEmpty(){
         return length==0;
@@ -22,42 +23,99 @@ public class DoublyLinkedList {
     public int length(){
         return length;
     }
-    public void displayForward(ListNode head){
+
+    public void displayForward(){
         if(head==null){
             return;
         }
         ListNode temp = head;
-        while (temp!=null){
+        while(temp!=null){
             System.out.print(temp.data+"-->");
             temp=temp.next;
         }
-        System.out.print("null");
+        System.out.print("null \n");
     }
 
-    public void displayBackward(ListNode tail){
+    public void displayBackward(){
         if(tail==null){
             return;
         }
         ListNode temp = tail;
-        while (temp!=null){
+        while(temp!=null){
             System.out.print(temp.data+"-->");
             temp=temp.previous;
         }
-        System.out.print("null");
+        System.out.print("null \n");
+    }
+
+    public void insertFirst(int value){
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            tail=newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+            length++;
+        }
+    }
+
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            head=newNode;
+        }
+        else{
+            tail.next = newNode;
+            newNode.previous = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+
+    public ListNode deleteFirst(){
+
+        ListNode temp = head;
+        if(head==tail){
+            tail=null;
+        }
+        else{
+            head.next.previous=null;
+        }
+        head=head.next;
+        temp.next=null;
+        length--;
+        return temp;
+    }
+
+    public ListNode deleteLast(){
+        ListNode temp = tail;
+        if(head==tail){
+            head=null;
+        }
+        else{
+            tail.previous.next=null;
+        }
+        tail=tail.previous;
+        temp.previous = null;
+        length--;
+        return temp;
     }
 
 
-    public static void main(String[] args) {
-       DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
-        doublyLinkedList.head = new ListNode(10);
-        ListNode second = new ListNode(20);
-        ListNode third = new ListNode(30);
-        ListNode fourth = new ListNode(40);
-        doublyLinkedList.head.next=second;
-        second.next=third;
-        third.next=fourth;
-        doublyLinkedList.displayForward(doublyLinkedList.head);
-        doublyLinkedList.displayBackward(doublyLinkedList.tail);
+    public static void main(String args[]){
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertFirst(1);
+        dll.insertFirst(10);
+        dll.displayForward();
+        dll.insertLast(20);
+        dll.insertLast(40);
+        dll.insertLast(60);
+        dll.displayForward();
+        dll.displayBackward();
+        dll.deleteFirst();
+        dll.deleteLast();
+        dll.displayForward();
 
     }
 }
